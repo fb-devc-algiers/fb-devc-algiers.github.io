d50e452c45e27bbd67c6c572348c29be613beb33
Mahfoudh AROUS <aladin02dz@hotmail.fr>
2019-01-31 10:53:23 +0100

    Prepare deploy

---
+++ precache-manifest.825e603c8b96183d3c4915f2c4b1a69a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "76dd37260cf7b7e1644e",
+    "url": "/static/js/main.76dd3726.chunk.js"
+  },
+  {
+    "revision": "b4a0c8291ae6662d7ee6",
+    "url": "/static/js/1.b4a0c829.chunk.js"
+  },
+  {
+    "revision": "76dd37260cf7b7e1644e",
+    "url": "/static/css/main.94e27b2f.chunk.css"
+  },
+  {
+    "revision": "758e0bb4a6b5db6efd7c61c281497335",
+    "url": "/index.html"
+  }
+];
